645116d19badd2bf3eb3413dc41f7776a8750810
khrom <khrom@DESKTOP-DA2UCIR>
2024-11-26 22:10:42 +0200

    Добавьте файлы проекта.

---
+++ Strokovoy Tip Dannih DZ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите любой текст:");
+        string input = Console.ReadLine(); 
+
+        bool containsDigit = false;
+
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                containsDigit = true;
+                break;
+            }
+        }
+     
+        if (containsDigit)
+        {
+            Console.WriteLine("Ну типа вроде как есть цифра хэ прикол");
+        }
+        else
+        {
+            Console.WriteLine("Нету тут ничего Андрей Валентинович ну ёмаё");
+        }
+    }
+}
